c2e3c54756f1f9fbf9d7f2d6869d991e987c614d
czimbortibor <czimbortibor@gmail.com>
2016-12-17 10:24:20 +0200

    added basic lock & signal thread mechanism

--- TCPServer/ClientThread.cpp
+++ TCPServer/ClientThread.cpp
@@ -9,7 +9,10 @@
 #include "ClientThread.h"
 
 ClientThread::ClientThread(ThreadArgs& threadArgs): MyThread(threadArgs), threadArgs(&threadArgs) {
-    // std::cout << this->threadArgs->acceptSocket << " " << this->threadArgs->messageBuff << "\n";
+    /** initializing mutex and condition variables */
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&condition, NULL);
+    
 }
 
 ClientThread::~ClientThread() {
@@ -17,28 +20,51 @@ ClientThread::~ClientThread() {
         delete threadArgs;
         threadArgs = NULL;
     }
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&condition);
+}
+
+void ClientThread::lockMutex() {
+    pthread_mutex_lock(&mutex);
+}
+
+void ClientThread::unlockMutex() {
+    pthread_mutex_unlock(&mutex);
+}
+
+void ClientThread::signalCondition() {
+    pthread_cond_signal(&condition);
 }
 
 void* ClientThread::run() {
     std::string errorMsg;
-    printf("Preparing the message for the client...\n");
-    int acceptSocket = threadArgs->acceptSocket;
-    int sizeOfMsg = sizeof(threadArgs->messageBuff);
-    char sendBuff[sizeOfMsg];
-   
-    // std::memcpy(sendBuff, threadData->sendBuff, lenOfBuff+1);
     
-    /** get the current system date + time */
-    time_t serverTime;
-    serverTime = time(NULL);
-    snprintf(sendBuff, sizeOfMsg, "%.24s", ctime(&serverTime));
+    /** lock the mutex and wait for signal */
+    pthread_mutex_lock(&mutex);
+    while(!loggedIn) {
+        /** automatically and atomically unlocks the mutex while it waits */
+        pthread_cond_wait(&condition, &mutex);
+        std::cout << "Preparing the message for the client...\n";
+        int acceptSocket = threadArgs->acceptSocket;
+        int sizeOfMsg = sizeof(threadArgs->messageBuff);
+        char sendBuff[sizeOfMsg];
 
-    printf("Sending the date/time to the client...\n");
-    int res = send(acceptSocket, sendBuff, strlen(sendBuff), 0);
-    if (res < 0) {
-        errorMsg = "Error while sending the message to the client!";
-        error(errorMsg.c_str());
+        // std::memcpy(sendBuff, threadData->sendBuff, lenOfBuff+1);
+
+        /** get the current system date + time */
+        time_t serverTime;
+        serverTime = time(NULL);
+        snprintf(sendBuff, sizeOfMsg, "%.24s", ctime(&serverTime));
+
+        std::cout << "Sending the date/time to the client...\n";
+        int res = send(acceptSocket, sendBuff, strlen(sendBuff), 0);
+        if (res < 0) {
+            errorMsg = "Error while sending the message to the client!";
+            error(errorMsg.c_str());
+        }
+        std::cout << "Message sent to the client.\n" ;
+        
     }
-    printf("Message sent to the client.\n");
+    pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
 } 
